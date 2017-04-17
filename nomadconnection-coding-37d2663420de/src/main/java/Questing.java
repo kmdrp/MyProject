0ea688d414cedb31b1bb6a5d058a1a161046c07d
keemmaang <keemmaang@keemmaangs-MacBook-Pro.local>
2017-04-17 15:57:24 +0900

    folder change

--- nomadconnection-coding-37d2663420de/src/main/java/Questing.java
+++ nomadconnection-coding-37d2663420de/src/main/java/Questing.java
@@ -1,46 +1,39 @@
+import java.util.Stack;
+
 /**
  * 테스트 코드를 보고, 아래 두 method 를 작성해주세요.
  */
 class Questing {
+
 	static int quest1(double d)  {
 		// FIXME
 		int result = ((int)Math.floor(d)/2);
-
-
 		return result;
 	}
 
 	static boolean quest2(String s) {
 		// FIXME
-		int large=0,mid=0,small=0;
-
-		char[] braces = s.toCharArray();
-		for(int i=0;i<braces.length;i++) {
-			if(braces[i]=='['){
-				large++;
-
-			}else if (braces[i] == '{') {
-				mid++;
-			}else if(braces[i]=='('){
-				small++;
-			}else if(braces[i]==']'){
-				if(large>0){
-					large--;
-				}else{
-					break;
-				}
-					}else if(braces[i]=='}'){
-				if(mid>0){mid--;}else{break;}
-			}else if(braces[i]==')'){
-				if(small>0){small--;}else{break;}
+		Stack<Character> stack = new Stack();
+		boolean isPair=true;
+		int i;
+		char[] input = s.toCharArray();
+		for(i=0;i<input.length;i++) {
+			char ch=input[i];
+			switch(ch){
+				case '[':stack.push(ch);break;
+				case '{':stack.push(ch);break;
+				case '(':stack.push(ch);break;
+				case ']':if(stack.isEmpty()||(ch=stack.pop())!='[')isPair=false;break;
+				case '}':if(stack.isEmpty()||(ch=stack.pop())!='{')isPair=false;break;
+				case ')':if(stack.isEmpty()||(ch=stack.pop())!='(')isPair=false;break;
 			}
-
+			if(!isPair)break;
 		}
-
-		if(large==0&&mid==0&&small==0){
-			return true;
+		if(stack.isEmpty()&&i==input.length){
+			isPair= true;
 		}else{
-			return false;
+			isPair=false;
 		}
+		return isPair;
 	}
 }
